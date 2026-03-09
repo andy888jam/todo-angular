3ff79126bd3b9861b1a77c78c17e2b352fbfd526
andy888jam <andy888jam@gmail.com>
2026-03-09 16:11:07 +0800

    User component management

--- README.md
+++ README.md
@@ -1,57 +1,146 @@
 # Note
 
 ## app module
+
 ### Concept
+
 Component setting in ts
 
 Module vs Standalone Component
 
-@for vs *ngFor(Old)
+@for vs \*ngFor(Old)
 
-@if vs *ngIf(Old)
+@if vs \*ngIf(Old)
 
 ### Property binding
+
 parent to child
 
- `<app-user
+`<app-user
         [user]="user"
         [selected]="user.id === selectedUserId"
       />`
 
 ### Event binding
+
 child to parent
 
 $event is the default event variable
 
- `<app-user
+`<app-user
           (select)="onSelectUser($event)"
       />`
 
+### Zone.js vs Signals
 
-# Essentials
+| 特性     | 傳統機制 (Zone.js + Dirty Checking)                                          | 新機制 (Signals)                                             |
+| -------- | ---------------------------------------------------------------------------- | ------------------------------------------------------------ |
+| 檢測範圍 | 組件樹級別。只要發生事件（如點擊），Angular 通常會從根部開始檢查整個組件樹。 | 原子級別。直接定位到受影響的 DOM 節點。                      |
+| 精準度   | 較低。即使只有一個數字變了，可能也要重新檢查幾十個組件。                     | 極高（Fine-grained）。只更新與該 Signal 綁定的特定 UI 片段。 |
+| 性能開銷 | 較高，隨著應用規模擴大，檢查負擔會增加。                                     | 較低，跳過了不必要的檢查邏輯。                               |
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.1.2.
+### 「傳統變數 + Getter」轉向「Signal + Computed」的核心進化
 
-## Development server
+#### 傳統屬性定義
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+##### 宣告
 
-## Code scaffolding
+` selectedUser = DUMMY_USERS[randomIndex];`
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+##### 私有變數
 
-## Build
+only available in this class cannot be accessed in template:
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
+` private selectedUser = DUMMY_USERS[randomIndex];`
 
-## Running unit tests
+##### getter
 
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+when using getter, you use it like a property, not a method, which means you can use it in template without parentheses
 
-## Running end-to-end tests
+```typescript
+get imagePath() {
+  return `assets/users/${this.selectedUser.avatar}`;
+}
+```
 
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
+##### 重新賦值
 
-## Further help
+`this.selectedUser = DUMMY_USERS[randomIndex];`
 
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+#### 新版signal屬性定義
+
+##### 宣告
+
+`selectedUser_new = signal(DUMMY_USERS[randomIndex]);`
+
+##### computed
+
+only compute the image path when the Signal used inside changes
+
+```typescript
+imagePath = computed(() => `assets/users/${this.selectedUser_new().avatar}`);
+```
+
+##### 重新賦值
+
+signal set method is used to update the value of the signal
+
+this.selectedUser_new.set(DUMMY_USERS[randomIndex]);
+
+### Interface vs Type
+
+outsource type: 將type移動到model中
+
+model.ts專門用來定義資料結構
+
+most of the time,interface and type are interchangeable, but in Angular project we use "interface" more often
+
+`type User = {
+ id: string;
+ avatar: string;
+ name: string;
+ }`
+
+interface can only define object type but type can define any type
+
+`interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}`
+
+### @Input (父傳子Data In) vs @Output (子傳父Event Out)
+
+#### 舊版
+
+! 是 TypeScript 的「確定賦值斷言」，告訴編譯器：「雖然我現在沒給它初始值，但我保證 Angular 在執行時一定會透過 Input 給它值，你別擔心。」
+
+`@Input({ required: true }) selected!: boolean;`
+
+EventEmitter 是一個發射器，專門用來「廣播」事件。 <string> 代表這個事件發射出去時，會夾帶一個 string 型態的資料（通常是 user.id）
+
+`@Output() select = new EventEmitter<string>();`
+
+`this.select.emit(this.user.id);`
+
+#### 新版
+
+input as a signal (function)
+
+- <> is a typescript thing, eg <T> is a generic type, T is a placeholder for a type
+
+- If it is required, you can't set the initial value, you have to "set it" in the template
+
+- You can't change the value of the input, you have to set it in the parents' template
+
+`avatar = input.required<string>();`
+
+`name = input.required<string>();`
+
+Output
+
+- Notice that output doesn't create a signal like input, it is an EventEmitter // the template still use the same way
+
+`select_new = output<string>();`
+
+`this.select_new.emit(this.user.id);`
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,19 @@
-import { Component, Input, Output, EventEmitter, output } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  output,
+  input,
+  computed,
+  signal,
+} from '@angular/core';
 
 // you can add "type" keyword to make it more readable
 import { type User } from './user.model';
+import { DUMMY_USERS } from '../dummy-user';
 
-// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 // // outsource type
 // most of the time,interface and type are interchangeable, but in Angular project we use interface more often
@@ -26,56 +36,66 @@ import { type User } from './user.model';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
-// we can access selectedUser in template
-// ! is used to tell TypeScript that the value is not null or undefined
-// required: true is used to tell Angular that the value is required, without this, you are lying to TypeScript as it can be undefined or null
 export class UserComponent {
-  // Input as a decorator
-  // @Input({ required: true }) id!: string;
-  // @Input({ required: true }) avatar!: string;
-  // @Input({ required: true }) name!: string;
+  // 1. Old version syntax of Input and Output
 
-  // define object type
+  // ! is used to tell TypeScript that the value is not null or undefined
+  // required: true is used to tell Angular that the value is required. Without this word, you are lying to TypeScript as it can be undefined or null
   @Input({ required: true }) user!: User;
   @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
-  // way2: output() (Notice that output doesn't create a signal like input, it is an EventEmitter)  // the template still use the same way
-  // select = output<string>();
+  // 2. New version syntax of input (signal) and output (not signal still)
 
   // input as a signal(function)
-  //<> is a typescript thing, eg <T> is a generic type, T is a placeholder for a type
-  // if it is required you can't set the initial value, you have to "set it" in the template
-  // can't change the value of the input, you have to set it in the template(read only)
-  // avatar = input.required<string>();
-  // name = input.required<string>();
+  // <> is a typescript thing, eg <T> is a generic type, T is a placeholder for a type
+  // If it is required, you can't set the initial value, you have to "set it" in the parents template
+  // You can't change the value of the input, you have to set it in the parents' template
+  avatar = input.required<string>();
+  name = input.required<string>();
 
-  // imagePath = computed(() => `assets/users/${this.avatar()}`);
+  // Notice that output doesn't create a signal like input, it is an EventEmitter  // the template still use the same way
+  select_new = output<string>();
+
   get imagePath() {
     return `assets/users/${this.user.avatar}`;
   }
+
+  // If using Signal, you should use computed:
+  imagePath_new = computed(() => `assets/users/${this.avatar()}`);
 
   onSelectUser() {
     this.select.emit(this.user.id);
   }
 }
 
-// // we can access selectedUser in template
-// export class UserComponent {
-//   // selectedUser = DUMMY_USERS[randomIndex];
-//   selectedUser = signal(DUMMY_USERS[randomIndex]);
-//   // only compute the image path when the Signal used inside changes
-//   imagePath = computed(() => `assets/users/${this.selectedUser().avatar}`);
-//   // private selectedUser = DUMMY_USERS[randomIndex]; // only available in this class cannot be accessed in template
+export class UserComponent2 {
+  ///// Define a property that can be used in the template
 
-//   // when using getter, you use it like a property, not a method, which means you can use it in template without parentheses
-//   // get imagePath() {
-//   //   return `assets/users/${this.selectedUser.avatar}`;
-//   // }
+  //traditional way to define a property that can be used in the template
+  selectedUser = DUMMY_USERS[randomIndex];
+  // private selectedUser = DUMMY_USERS[randomIndex]; // only available in this class cannot be accessed in template
 
-//   onSelectUser() {
-//     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-//     this.selectedUser.set(DUMMY_USERS[randomIndex]);
-//     // this.selectedUser = DUMMY_USERS[randomIndex];
-//   }
-// }
+  // when using signal, you need to call it like a function to get the value
+  selectedUser_new = signal(DUMMY_USERS[randomIndex]);
+
+  //// Getter vs Computed
+
+  // when using getter, you use it like a property, not a method, which means you can use it in template without parentheses
+  get imagePath_old() {
+    return `assets/users/${this.selectedUser.avatar}`;
+  }
+  // only compute the image path when the Signal used inside changes
+  imagePath = computed(() => `assets/users/${this.selectedUser_new().avatar}`);
+
+  //// Update the value of the property
+  //Event Handler
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+    this.selectedUser = DUMMY_USERS[randomIndex];
+
+    // signal set method is used to update the value of the signal
+    this.selectedUser_new.set(DUMMY_USERS[randomIndex]);
+  }
+}
